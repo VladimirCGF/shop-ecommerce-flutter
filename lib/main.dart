0d4e74276fcba938a8340bfa4f9f3e1fb6468a68
Vladimir Coutinho <vladimircoutinho@unitins.br>
2025-05-07 18:23:09 -0300

    FireBase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/models/cart.dart';
@@ -12,10 +13,15 @@ import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/pages/products_page.dart';
 import 'package:shop/utils/app_routes.dart';
 
+import 'firebase_options.dart';
 import 'models/auth.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 
 class Auth with ChangeNotifier {
   static const _url =
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]';
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDlv-_stL0YT06-fE0EtmxIhUAnUJEHTRo'; // URL alterada para signUp'
 
   Future<void> signup(String email, String password) async {
     final response = await http.post(
