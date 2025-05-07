716fc68b9a523eccc6943427f70ca704be593a7f
Vladimir Coutinho <vladimircoutinho@unitins.br>
2025-05-07 20:12:05 -0300

    Usando ProxyProvider para compartilhar o Token

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -8,7 +8,8 @@ import 'package:shop/models/product.dart';
 import 'package:shop/utils/constants.dart';
 
 class ProductList with ChangeNotifier {
-  final List<Product> _items = [];
+  String _token;
+  List<Product> _items = [];
   final _baseUrl = 'https://shop-43627-default-rtdb.firebaseio.com/products';
 
   List<Product> get items => [..._items];
@@ -16,13 +17,17 @@ class ProductList with ChangeNotifier {
   List<Product> get favoriteItems =>
       _items.where((prod) => prod.isFavorite).toList();
 
+  ProductList(this._token, this._items);
+
   int get itemsCount {
     return _items.length;
   }
 
   Future<void> loadProducts() async {
     _items.clear();
-    final response = await http.get(Uri.parse('${Constants.PRODUCT_BASE_URL}.json'),);
+    final response = await http.get(
+      Uri.parse('${Constants.PRODUCT_BASE_URL}.json?auth=$_token'),
+    );
     if (response.body == 'null') return;
     Map<String, dynamic> data = jsonDecode(response.body);
     data.forEach((productId, productData) {
