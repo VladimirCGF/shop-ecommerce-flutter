e3a07c1d4de8cdc457dd1144e829cf0a3fe08e79
Vladimir Coutinho <vladimircoutinho@unitins.br>
2025-05-08 17:54:15 -0300

    Logout automático

--- lib/components/app_drawer.dart
+++ lib/components/app_drawer.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/auth.dart';
 import 'package:shop/utils/app_routes.dart';
 
 class AppDrawer extends StatelessWidget {
@@ -43,6 +45,20 @@ class AppDrawer extends StatelessWidget {
               );
             },
           ),
+          const Divider(),
+          ListTile(
+            leading: const Icon(Icons.exit_to_app),
+            title: const Text('Sair'),
+            onTap: () {
+              Provider.of<Auth>(
+                context,
+                listen: false,
+              ).logout();
+              Navigator.of(context).pushReplacementNamed(
+                AppRoutes.authOrHome,
+              );
+            },
+          )
         ],
       ),
     );
