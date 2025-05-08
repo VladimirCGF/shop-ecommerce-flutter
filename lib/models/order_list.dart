e5c729fa3b6c866f3af5af067b1c04b2caf8d413
Vladimir Coutinho <vladimircoutinho@unitins.br>
2025-05-08 17:21:50 -0300

    Associar pedido ao usuário

--- lib/models/order_list.dart
+++ lib/models/order_list.dart
@@ -11,9 +11,14 @@ import '../utils/constants.dart';
 
 class OrderList with ChangeNotifier {
   final String _token;
+  final String _userId;
   List<Order> _items = [];
 
-  OrderList(this._token, this._items);
+  OrderList([
+    this._token = '',
+    this._userId = '',
+    this._items = const [],
+  ]);
 
   List<Order> get items {
     return [..._items];
@@ -27,9 +32,10 @@ class OrderList with ChangeNotifier {
     List<Order> items = [];
 
     final response = await http.get(
-      Uri.parse('${Constants.ORDER_BASE_URL}.json?auth=$_token'),
+      Uri.parse('${Constants.ORDER_BASE_URL}/$_userId.json?auth=$_token'),
     );
     if (response.body == 'null') return;
+
     Map<String, dynamic> data = jsonDecode(response.body);
     data.forEach((orderId, orderData) {
       items.add(
@@ -55,7 +61,7 @@ class OrderList with ChangeNotifier {
   Future<void> addOrder(Cart cart) async {
     final date = DateTime.now();
     final response = await http.post(
-      Uri.parse('${Constants.ORDER_BASE_URL}.json?auth=$_token'),
+      Uri.parse('${Constants.ORDER_BASE_URL}/$_userId.json?auth=$_token'),
       body: jsonEncode(
         {
           'total': cart.totalAmount,
@@ -73,10 +79,11 @@ class OrderList with ChangeNotifier {
       ),
     );
 
+    final id = jsonDecode(response.body)['name'];
     _items.insert(
       0,
       Order(
-        id: Random().nextDouble().toString(),
+        id: id,
         total: cart.totalAmount,
         date: date,
         products: cart.items.values.toList(),
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,6 @@
 class Constants {
+  static const USER_FAVORITES_BASE_URL =
+      'https://shop-43627-default-rtdb.firebaseio.com/userFavorites';
   static const PRODUCT_BASE_URL =
       'https://shop-43627-default-rtdb.firebaseio.com/products';
   static const ORDER_BASE_URL =
